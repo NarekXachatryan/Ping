00c33a534c52e48d75f091707130df4c61a228ce
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-09-06 19:08:49 +0400

    Add files via upload

---
+++ ProjectForPing/ProjectForPing/ClassPing.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+
+namespace ProjectForPing
+{
+    class ClassPing
+    {
+        [Obsolete]
+        public static void Ping()
+        {
+            string ip = ConfigurationSettings.AppSettings.Get("Ip");
+            string filePath = ConfigurationSettings.AppSettings.Get("LogFilePath");
+            Ping p1 = new Ping();
+            PingOptions pingOptions = new PingOptions();
+
+            PingReply PR;
+
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (StreamWriter strWr = new StreamWriter(fs))
+                {
+                    while (true)
+                    {
+                        fs.Position = fs.Length + 1;
+                        Thread.Sleep(1000);
+                        PR = p1.Send(ip);
+                        if (PR.Status == IPStatus.Success)
+                        {
+
+                            strWr.WriteLine($"{DateTime.Now}  Ip:{ip} - Ok");
+
+                        }
+                        else
+                        {
+                            strWr.WriteLine($"{DateTime.Now}  Ip:{ip} - Failed");
+                        }
+                        Console.WriteLine($"{DateTime.Now} - {PR.Status}");
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ ProjectForPing/ProjectForPing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectForPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ClassPing.Ping();
+        }
+    }
+}
